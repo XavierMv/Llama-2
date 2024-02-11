1eb8804e59cbf3099f5f30189d7e1a895c1c4ef0
XavierMv <xaviermv96@gmail.com>
2024-02-11 10:43:27 -0500

    updated script

---
+++ llama_2_script.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def gpt_chat():
+    st.title("Chat with Llama")
+    
+
+def gpt_chat_spanish():
+    st.title("We will be using Llama... para hablar en español!")
+
+def llama2():
+    st.title("Llama")
